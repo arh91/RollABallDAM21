f0515f111c4ac052c877f974ec9b7721848a267b
arh91 <alvaro_rh_991@hotmail.com>
2023-02-21 15:13:51 +0100

    Modifico la velocidad ( variable speed) para que el GameObject se mueva más rápido. También añado comentarios al código para explicar el funcionamiento del mismo.

--- PlayerController.cs
+++ PlayerController.cs
@@ -6,18 +6,19 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
 
-    public float speed = 1;
-    private Rigidbody rb;
+    public float speed = 8; //Cambio la velocidad a 8 para que el Object se mueva más rápido
+    private Rigidbody rb; //Rigidbody es una clase que permite que el GameObject esté sometido a las leyes de la física (gravedad, rozamiento, colisión con otros objetos...).
     private float movementX;
     private float movementY;
     
     
-    // Start is called before the first frame update
+    // Este método inicializa el Object. Sólo se llama si el GameObject está previamente activado.
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
+    //Establecemos que el Object se moverá en el eje X y en el eje Y.
     void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
@@ -26,10 +27,12 @@ public class PlayerController : MonoBehaviour
         movementY = movementVector.y;
     }
     
+    /*Método que será llamado en cada frame. Todas las actualizaciones físicas (paso del motor físico)
+    se llevarán a cabo tras la ejecución de este método*/
     void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
 
-        rb.AddForce(movement*speed);
+        rb.AddForce(movement * speed);
     }
 }
